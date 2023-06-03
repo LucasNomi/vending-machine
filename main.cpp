59ed39b6c457b4565fb050e04742b99d6b35eef2
LucasNomi <lucas04.dev@gmail.com>
2023-06-02 21:27:02 -0300

    feat: funcoes produto

--- main.cpp
+++ main.cpp
@@ -15,11 +15,54 @@ struct produto
     float precoProduto;
     char *nomeProduto;
 
-    produto() : qtdProduto(0), precoProduto(0), nomeProduto("produto") {}
+    produto() : qtdProduto(1), precoProduto(1), nomeProduto("produto") {}
+
+    // User functions
+    float Purchase(float dinheiro)
+    {
+        float troco;
+
+        if (dinheiro >= precoProduto)
+        {
+            qtdProduto--;
+            troco = dinheiro - precoProduto;
+            return troco;
+        }
+        else
+        {
+            std::cout << "Nao foi possivel comprar o produto! \n";
+            return dinheiro;
+        }
+    }
+
+    // Adm functions
+    void Restock()
+    {
+        qtdProduto = maxQtdProduto;
+    }
+
+    float Faturamento()
+    {
+        float faturamento = 0;
+        faturamento = (maxQtdProduto - qtdProduto) * precoProduto;
+
+        return faturamento;
+    }
+
+    float FaturamentoFuturo()
+    {
+        float faturamentoFuturo;
+        float faturamentoTotal = maxQtdProduto * precoProduto;
+        float faturamentoAtual = Faturamento();
+
+        faturamentoFuturo = faturamentoTotal - faturamentoAtual;
+
+        return faturamentoFuturo;
+    }
 };
 
-int main() {
-
+int main()
+{
     return 0;
 }
 
